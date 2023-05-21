362cc30db3978a3c57f548d0a06d4ca51e5ecad6
Stephen Kirby <me@skirby.dev>
2023-05-21 08:30:25 -0600

    add documentation to site struct

--- site.go
+++ site.go
@@ -1,16 +1,19 @@
 package main
 
+//Site represents a HostSplitter site
 type Site struct {
 	backendIndex int
 	Backends     []string
 	Secret       string
 }
 
+//GetBackend returns the next backend according using round robin
 func (this *Site) GetBackend() string {
 	if len(this.Backends) == 0 {
 		return ""
 	}
 
+	//This code is racy but there's not much reason to make it synchronized
 	index := this.backendIndex
 
 	if this.backendIndex == len(this.Backends)-1 {
