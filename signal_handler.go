adf24b1d57edbb523e2f9e23a0dac8aa7de44721
Stephen Kirby <me@skirby.dev>
2023-05-25 11:40:40 -0600

    move signal wait loop into it's own goroutine

--- signal_handler.go
+++ signal_handler.go
@@ -10,8 +10,11 @@ import (
 func init() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.Signal(0xa))
-	for syscall.Signal(0xa) == <-sigs {
-		log.Print("Recieved 0xa, reloading config")
-		LoadConfig()
-	}
+	go func() {
+		for syscall.Signal(0xa) == <-sigs {
+			log.Print("Recieved 0xa, reloading config")
+			LoadConfig()
+		}
+	}()
+
 }
